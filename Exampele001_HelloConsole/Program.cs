1a5af3a526b5786a2680dd71442a19ab7679a5be
creems84 <128843897+creems84@users.noreply.github.com>
2023-04-20 22:02:06 +0300

    add Example 001 Hello Console project

---
+++ Exampele001_HelloConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exampele001_HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World and You!"); 
+            
+        }
+    }
+}
